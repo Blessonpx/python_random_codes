cdf026588aef0f791b751090ab0817bfa33db910
Blesson <blessonxavierofficial@gmail.com>
2023-12-14 22:24:28 +0530

    day 1

--- implement_single_linkList.py
+++ implement_single_linkList.py
@@ -1,6 +1,30 @@
-def my_function():
-    print("my_function()")
+class Node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+    def set_data(self,data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+    
+    def set_next(self,next):
+        self.next = next
+    
+    def get_next(self):
+        return self.next
+    
+    def has_next(self):
+        return self.next != None
+    
 
 if __name__== '__main__':
     print("Hello!")
-    my_function()
+    x= Node()
+    x.set_data(5)
+    y = Node()
+    y.set_data(4)
+    x.set_next(y)
+    print(x.get_data())
+    print(x.get_next().get_data())
