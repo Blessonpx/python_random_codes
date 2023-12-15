f5d2ba9905eab2d4287dcca2d7ceece7d4fea17c
Blesson <blessonxavierofficial@gmail.com>
2023-12-15 22:29:53 +0530

    Day 2

--- implement_single_linkList.py
+++ implement_single_linkList.py
@@ -18,9 +18,30 @@ class Node:
     def has_next(self):
         return self.next != None
     
+class Slinked_List:
+    def __init__(self) :
+        self.head=None
+    
+    def get_length(self):
+        current = self.head
+        count=0
+        while current!=None:
+            count=count+1
+            current=current.get_next()
+        return count
+    
 
 if __name__== '__main__':
     print("Hello!")
+    List = Slinked_List()
+    List.head = Node()
+    List.head.set_data("Mon")
+    n2=Node()
+    n2.set_data("Tue")
+    List.head.set_next(n2)
+    n3=Node()
+    n3.set_data("Tue")
+    n2.set_next(n3) 
     x= Node()
     x.set_data(5)
     y = Node()
@@ -28,3 +49,5 @@ if __name__== '__main__':
     x.set_next(y)
     print(x.get_data())
     print(x.get_next().get_data())
+    print("Length--")
+    print(List.get_length())
